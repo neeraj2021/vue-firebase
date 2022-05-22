f3ffab924162bd2332199dc67fa1ac474c54512f
kumar-neeraj-001 <98804538+kumar-neeraj-001@users.noreply.github.com>
2022-05-22 17:58:44 +0530

    add moment plugin

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Today Date using new Date = {{ date }}</h1>
+    <h1>Today Date using moment = {{ momentDate }}</h1>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+// import moment from "moment";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  data() {
+    return {
+      date: new Date(),
+      momentDate: moment.weekdays(),
+    };
+  },
+});
 </script>
